4b68c7f8374bec1dcd72a789ea01363a22b91a33
OLIVIA LAM <aqdp2d@gmail.com>
2024-12-25 14:37:25 -0600

    I guess we are using classes

--- src/types/BoardState.ts
+++ src/types/BoardState.ts
@@ -1,10 +1,25 @@
 import { PlayingCard } from ".";
 
-export interface BoardState {
-    cardsInPlay: null
-        | [PlayingCard]
-        | [PlayingCard, PlayingCard]
-        | [PlayingCard, PlayingCard, PlayingCard]
-        | [PlayingCard, PlayingCard, PlayingCard, PlayingCard]
-        | [PlayingCard, PlayingCard, PlayingCard, PlayingCard, PlayingCard]
+// export interface BoardStateInterface {
+//     cardsInPlay: null
+//         | []
+//         | [PlayingCard]
+//         | [PlayingCard, PlayingCard]
+//         | [PlayingCard, PlayingCard, PlayingCard]
+//         | [PlayingCard, PlayingCard, PlayingCard, PlayingCard]
+//         | [PlayingCard, PlayingCard, PlayingCard, PlayingCard, PlayingCard]
+// }
+
+type ValidHand = [] | [PlayingCard] | [PlayingCard, PlayingCard]| [PlayingCard, PlayingCard, PlayingCard]| [PlayingCard, PlayingCard, PlayingCard, PlayingCard]| [PlayingCard, PlayingCard, PlayingCard, PlayingCard, PlayingCard]
+
+export class BoardState {
+    cardsInPlay: ValidHand
+
+    constructor(initialCardsInPlay: ValidHand = []) {
+        this.cardsInPlay = initialCardsInPlay
+    }
+
+    evaluateHand() {
+        
+    }
 }
